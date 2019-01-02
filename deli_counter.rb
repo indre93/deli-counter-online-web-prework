47b8bd84a146db68da77c2c2ca4268217c35642a
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-02 04:12:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+katz_deli = []
+other_deli = ["Logan", "Avi", "Spencer"]
+another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+
+
+def line(katz_deli)
+  puts "The line is currently empty."
+ end
+ other_deli.concat(another_deli)
+ puts ""
+
+
+
+end
