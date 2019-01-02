6e6e4c3feb5883a6a990d118015102791c1ddb3f
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-02 05:20:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,10 +7,9 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else katz_deli.each_with_index do |name, index|
-    katz_deli.push("#{index}. #{name}")
+    katz_deli.push("#{index + 1}. #{name}")
   end
-end
-puts 
+  puts "The line is currently"
 end
 
 
