b688063efdc6f88a3693cb4b735adf3f63b89fd6
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-02 04:56:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi"
 
 
 def line(katz_deli)
-  if katz_deli.lenght == 0
+  if katz_deli.length == 0
     puts "The line is currently empty."
   end
 end
