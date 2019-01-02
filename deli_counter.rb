9d74f1390a84db2a31d7f11eb106230517569000
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-02 04:58:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,11 +6,33 @@ another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi"
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
-  else katz_deli.each_with_index
+  else katz_deli.each_with_index{|name, index| katz_deli.push("")
   end
 end
 
 
+describe "#line" do
+    context "there is nobody in line" do
+      it "should say the line is empty" do
+        # This line checks the current standard output (your terminal screen)
+        # to make sure the correct output has been puts'ed.
+        expect($stdout).to receive(:puts).with("The line is currently empty.")
+        line(katz_deli)
+      end
+    end
+
+    context "there are people in line" do
+      it "should display the current line" do
+        expect($stdout).to receive(:puts).with("The line is currently: 1. Logan 2. Avi 3. Spencer")
+        line(other_deli)
+      end
+
+      it 'works with different people in line' do
+	      expect($stdout).to receive(:puts).with("The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey")
+	line(another_deli)
+      end
+    end
+  end
 
 
 
